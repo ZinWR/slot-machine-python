04a02cb7c9527d5cae7b16cb6e801defe3259d9a
QuanN <jeanquan98@gmail.com>
2024-04-19 22:47:51 -0500

    Built user input deposit function

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+print("Welcome to Slot Machine Game!")
+
+# get user input
+def deposit():
+    while True:
+        amount = input('What would you like to deposit? $')
+        if amount.isdigit(): # validation check
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else: 
+            print("Please enter a number.")
+    return amount
+        
