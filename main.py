60529e82a30ee72be88cc83811099a216549cf0c
QuanN <jeanquan98@gmail.com>
2024-04-19 23:04:03 -0500

    added logic to betting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 print("Welcome to Slot Machine Game!")
 
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 # get user input
 def deposit():
@@ -29,9 +31,30 @@ def get_lines():
             print("Please enter a number.")
     return lines
 
+def get_bet():
+    while True:
+        amount = input('What would you like to bet on each line? $')
+        if amount.isdigit(): # validation check
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} ${MAX_BET}.")
+        else: 
+            print("Please enter a number.")
+    return amount
+
 def main(): 
     balance = deposit()
     lines = get_lines()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount, your current balance is: ${balance}")
+        else: 
+            break
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
 
 main()
